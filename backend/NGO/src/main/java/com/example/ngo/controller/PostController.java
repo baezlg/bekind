ee0cebd5ef5039fc83f93a8cdd86cbe6a3cf86c9
amoloye <adedamolaadegoke2@gmail.com>
2022-11-20 19:07:24 +0200

    get all posts using offset and PageSize

--- backend/NGO/src/main/java/com/example/ngo/controller/PostController.java
+++ backend/NGO/src/main/java/com/example/ngo/controller/PostController.java
@@ -7,10 +7,12 @@ import com.example.ngo.entity.Post;
 import com.example.ngo.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
 @RestController
@@ -22,13 +24,21 @@ public class PostController {
     private final PostService postService;
 
 
-
     @PostMapping( value = {"/addNewPost"}, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<Post> addNewPost(@RequestPart("post") Post post){
         log.debug("REST request to create Post");
         return  ResponseEntity.ok().body(postService.addNewPost(post));
 
     }
+
+    @GetMapping(value = {"/posts/{offset}/{pageSize}"}, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<Page<Post>> searchPosts(@PathVariable("offset") Integer offset,
+                                                  @PathVariable("pageSize") Integer pageSize){
+        log.debug("get all posts");
+        return  ResponseEntity.ok().body(postService.searchPosts(offset,pageSize));
+
+    }
+
 
 
 }
